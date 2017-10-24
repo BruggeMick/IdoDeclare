30c6e9d7ee7c5b95f04426a6a2903ad86bbf3953
BruggeMick <bruggemick@gmail.com>
2017-10-24 11:07:12 -0500

    added files

---
+++ JavaDeclare.js
@@ -0,0 +1,17 @@
+    var yourName = "Fred";
+    var myName = "Mick";
+    var hisName = "Jim";
+    var herName = "Sally";
+    console.log(yourName, myName, hisName, herName);
+
+
+    var thisNumber = 7.35;
+    var thatNumber = 29;
+    var hisNumber = -72;
+    var herNumber = 55;
+    console.log(thisNumber, thatNumber,hisNumber, herNumber);
+
+
+    var Bamawins = true;
+    var Bamaloses = false;
+    console.log(Bamawins, Bamaloses);
